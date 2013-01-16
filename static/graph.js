fd50c3a2ae2c75205dc99f9eb6567be90e34b330
Stuart Knightley <stuart@stuartk.com>
2013-01-16 14:37:02 -0800

    Curved arrows!

--- static/graph.js
+++ static/graph.js
@@ -1,54 +1,83 @@
 (function (d3) {
 
-var width = 960,
-    height = 500;
-
-var force = d3.layout.force()
-    .charge(-2000)
-    .size([width, height]);
-
-var svg = d3.select("#chart").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-var globalLinks, link;
-
 d3.json("data.json", function(data) {
     var nodes = data.nodes, links = data.links;
 
-    force
+    var w = 960,
+        h = 500;
+
+    var force = d3.layout.force()
         .nodes(nodes)
         .links(links)
+        .size([w, h])
+        .linkDistance(60)
+        .charge(-300)
+        .on("tick", tick)
         .start();
 
-    link = svg.selectAll("line.link")
-        .data(links)
-      .enter().append("line")
-        .attr("class", "link");
+    var svg = d3.select("#chart").append("svg:svg")
+        .attr("width", w)
+        .attr("height", h);
 
-    var g = svg.selectAll("g.node")
-        .data(nodes)
-      .enter().append("svg:g")
-        .classed("node", true)
+    // Per-type markers, as they don't inherit styles.
+    svg.append("svg:defs").selectAll("marker")
+        .data(["suit", "licensing", "resolved"])
+      .enter().append("svg:marker")
+        .attr("id", String)
+        .attr("viewBox", "0 -5 10 10")
+        .attr("refX", 15)
+        .attr("refY", -1.5)
+        .attr("markerWidth", 6)
+        .attr("markerHeight", 6)
+        .attr("orient", "auto")
+      .append("svg:path")
+        .attr("d", "M0,-5L10,0L0,5");
+
+    var path = svg.append("svg:g").selectAll("path")
+        .data(force.links())
+      .enter().append("svg:path")
+        .attr("class", function(d) { return "link suit"; })
+        .attr("marker-end", function(d) { return "url(#suit)"; });
+
+    var circle = svg.append("svg:g").selectAll("circle")
+        .data(force.nodes())
+      .enter().append("svg:circle")
+        .attr("r", 6)
         .call(force.drag);
 
-    g.append("svg:circle")
-        .attr("r", 5);
-        // .style("fill", function(d, i) { return color(i); });
+    var text = svg.append("svg:g").selectAll("g")
+        .data(force.nodes())
+      .enter().append("svg:g");
 
-    g.append("svg:text")
-        .attr("x", 10)
-        .attr("dy", ".31em")
+    // A copy of the text with a thick white stroke for legibility.
+    text.append("svg:text")
+        .attr("x", 8)
+        .attr("y", ".31em")
+        .attr("class", "shadow")
         .text(function(d) { return d.id; });
 
-    force.on("tick", function() {
-      link.attr("x1", function(d) { return d.source.x; })
-          .attr("y1", function(d) { return d.source.y; })
-          .attr("x2", function(d) { return d.target.x; })
-          .attr("y2", function(d) { return d.target.y; });
+    text.append("svg:text")
+        .attr("x", 8)
+        .attr("y", ".31em")
+        .text(function(d) { return d.id; });
 
-      g.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+    // Use elliptical arc path segments to doubly-encode directionality.
+    function tick() {
+      path.attr("d", function(d) {
+        var dx = d.target.x - d.source.x,
+            dy = d.target.y - d.source.y,
+            dr = Math.sqrt(dx * dx + dy * dy);
+        return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+      });
 
-    });
-  });
+      circle.attr("transform", function(d) {
+        return "translate(" + d.x + "," + d.y + ")";
+      });
+
+      text.attr("transform", function(d) {
+        return "translate(" + d.x + "," + d.y + ")";
+      });
+    }
+});
+
 }(this.d3));
